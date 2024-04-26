dd22e156f1da1ab00e4b11ab74748bb1e47ac01e
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-04-26 12:25:03 +0200

    adding all the traversal method to the AVL

--- TreeAndAlgorithms/src/it/avltree/AVL.java
+++ TreeAndAlgorithms/src/it/avltree/AVL.java
@@ -1,5 +1,7 @@
 package it.avltree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class AVL {
 
@@ -13,11 +15,52 @@ public class AVL {
 		if(node == null) {
 			return;
 		}
-		System.out.println(node.value + " ");
+		System.out.print(node.value + " ");
 		preOrder(node.left);
 		preOrder(node.right);
 	}
 	
+	public void inOrder(BinaryNodeAVL node) {
+		if(node == null) {
+			return;
+		}
+		preOrder(node.left);
+		System.out.print(node.value + " ");
+		preOrder(node.right);		
+		
+	}
+	
+	public void postOrder(BinaryNodeAVL node) {
+		if(node == null) {
+			return;
+		}
+		preOrder(node.left);
+		preOrder(node.right);		
+		System.out.print(node.value + " ");
+		
+	}
 	
 	
+	public void levelOrder() {
+		if(root == null) {
+			return;
+		}
+		
+		Queue<BinaryNodeAVL> queue = new LinkedList<BinaryNodeAVL>();
+		
+		queue.add(root);
+		
+		while(!queue.isEmpty()) {
+			BinaryNodeAVL toPrint  = queue.remove();
+			System.out.print(toPrint.value + " ");
+			if(toPrint.left != null) {
+				queue.add(toPrint.left);
+			}
+			if(toPrint.right!= null) {
+				queue.add(toPrint.right);
+			}
+		}
+		
+		
+	}
 }
